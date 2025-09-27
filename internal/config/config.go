b4eadcdbaeb9bc42b56644cb055f67cada0dea03
Hoang Duc <hoangduc1662002@gmail.com>
2025-09-27 17:11:40 +0700

    feat: implement LRU eviction policy and enhance eviction pool management

--- internal/config/config.go
+++ internal/config/config.go
@@ -10,6 +10,9 @@ var EvictionRatio = 0.1
 
 var EvictionPolicy string = "allkeys-random"
 
+var EpoolMaxSize = 16
+var EpoolLruSampleSize = 5
+
 // var MaxWorkers = 10
 // var MaxQueue = 100
 // var MaxConnectionsPerIP = 10
